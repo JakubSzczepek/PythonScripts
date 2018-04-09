73310d574d4d55d50f9d4d02773ff04364bd9762
JakubSzczepek <jakub.szczepek@hotmail.com>
2018-04-09 19:49:31 +0200

    Add files via upload

---
+++ KsiazkaAdresowa.py
@@ -0,0 +1,57 @@
+from pprint import pprint
+
+
+class Kontakt:
+
+    def __init__(self, imie = None, nazwisko = None, adresy = [] ):
+        self.imie  = imie
+        self.nazwisko = nazwisko
+        self.adres = adresy
+
+    def pokaz_kontakt(self):
+        print(self.imie, self.nazwisko)
+        for item in self.adres:
+            item.pokaz_adres()
+
+    def miejsce_zamieszkania(self):
+        for item in self.adres:
+            item.pokaz_adres()
+
+    def pokaz_osobe(self):
+        print(self.imie, self.nazwisko)
+
+
+class Adres:
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+    def pokaz_adres(self):
+        for key in self.__dict__:
+            print("     {} {}".format(key, self.__dict__[key]))
+        #     # print('Adres: ' , list(self.__dict__.items()))
+
+
+
+ksiazka_adresowa = [
+     Kontakt(imie='Max', nazwisko='Peck', adresy=[
+        Adres(ulica='2101 E NASA Pkwy', miasto='Houston', stan='Texas', kod='77058', panstwo='USA'),
+        Adres(ulica=None, miasto='Kennedy Space Center', kod='32899', panstwo='USA'),
+        Adres(ulica='4800 Oak Grove Dr', miasto='Pasadena', kod='91109', panstwo='USA'),
+        Adres(ulica='2825 E Ave P', miasto='Palmdale', stan='California', kod='93550', panstwo='USA'),
+    ]),
+    Kontakt(imie='José', nazwisko='Jiménez'),
+    Kontakt(imie='Иван', nazwisko='Иванович', adresy=[]),
+]
+#
+ksiazka_adresowa[0].pokaz_kontakt()
+ksiazka_adresowa[1].pokaz_kontakt()
+ksiazka_adresowa[2].pokaz_kontakt()
+#
+ksiazka_adresowa[0].miejsce_zamieszkania()
+ksiazka_adresowa[1].miejsce_zamieszkania()
+ksiazka_adresowa[2].miejsce_zamieszkania()
+#
+ksiazka_adresowa[0].pokaz_osobe()
+ksiazka_adresowa[1].pokaz_osobe()
+ksiazka_adresowa[2].pokaz_osobe()
